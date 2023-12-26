bb0a29e3f26df0088b98d75c855d0acd0fd5eb78
Jahirul Islam <jahirulislamjantu002@gmail.com>
2023-12-26 22:08:43 +0600

    router created

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require("express");
 
 const app = express();
 
+app.get("hello", (req, res, next) => {
+  res.json({
+    message: "Hello",
+  });
+});
+
 app.listen(8000, () => {
   console.log("App is running on port 8000");
 });
