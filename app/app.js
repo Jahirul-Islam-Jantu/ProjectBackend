19d8003abf422dbf931ef415bc59a021688609be
Jahirul Islam <jahirulislamjantu002@gmail.com>
2023-12-29 05:15:35 +0600

    app clear

--- app/app.js
+++ app/app.js
@@ -1,31 +1,12 @@
 require("dotenv").config("../.env");
 const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
+const { errorHandler, notFoundHandler } = require("./error");
 
 const app = express();
 
-app.use([morgan("dev"), cors(), express.json()]);
-
-app.get("/health", (_req, res) => {
-  throw new Error("Error");
-  res.status(200).json({ message: "Success" });
-});
-
-app.use((_req, _res, next) => {
-  const error = new Error("Resource not found");
-  error.status = 404;
-  next(error);
-});
-app.use((error, _req, res, _next) => {
-  if (error.status) {
-    return res.status(error.status).json({
-      message: error.message,
-    });
-  }
-  res.status(500).json({
-    message: "Something went Wrong!",
-  });
-});
+app.use(require("./middleware"));
+app.use(require("./routs"));
+app.use(notFoundHandler);
+app.use(errorHandler);
 
 module.exports = app;
