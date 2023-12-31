874b3bb7ecb5386b91ea4f7e2d5c0b8a0e3a4868
Jahirul Islam <jahirulislamjantu002@gmail.com>
2023-12-31 22:52:09 +0600

    Port changed

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const http = require("http");
 const app = require("./app/app");
 
 const server = http.createServer(app);
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 
 server.listen(PORT, () => {
   console.log(`Server is listening on Port ${PORT}`);
